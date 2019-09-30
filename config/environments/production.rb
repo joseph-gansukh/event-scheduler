73ebdc9c84101ca5104b9da0503c872470105fa8
joseph-gansukh <g.sukhbat@gmail.com>
2019-09-30 13:14:40 -0500

    Added login feature

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "event_scheduler_production"
+  # config.active_job.queue_name_prefix = "event_snacks_production"
 
   config.action_mailer.perform_caching = false
 
